0648096193b1845fe59ab3ebcf5298ca4a5f6630
natthapach <natthapach.a@ku.th>
2017-09-05 23:07:36 +0700

    dispart initial instance between portrait and landscape

--- app/src/main/java/com/example/a708/lab3/MainActivity.java
+++ app/src/main/java/com/example/a708/lab3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a708.lab3;
 
 import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initInstance();
+        int display_mode = getResources().getConfiguration().orientation;
 
+        if (display_mode == Configuration.ORIENTATION_PORTRAIT) {
+            Log.d("lab3", "Portrait");
+        } else {
+            Log.d("lab3", "land");
+            initLandInstance();
+        }
     }
 
-    private void initInstance() {
+    private void initLandInstance() {
         increasePlayer1Btn = (Button) findViewById(R.id.inc_1_btn);
         decreasePlayer1Btn = (Button) findViewById(R.id.dec_1_btn);
 
@@ -41,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
     private void increasePoint(View view){
         String tag = (String) view.getTag();
         Log.d("click btn", tag);
+        View layout = findViewById(R.id.layout);
+
+        TextView textView = layout.findViewWithTag(tag);
+        int score = Integer.parseInt(textView.getText().toString());
+        score++;
+        textView.setText(score + "");
+        Log.d("lab3", score + "");
     }
 
     @Override
