3138d4f8af7b2209abd41a7331fa3d8ca28d3c98
natthapach <natthapach.a@ku.th>
2017-09-06 21:40:12 +0700

    complete increase, decrease and reset

--- app/src/main/java/com/example/a708/lab3/MainActivity.java
+++ app/src/main/java/com/example/a708/lab3/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.res.Configuration;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,10 +13,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button increasePlayer1Btn;
     private Button decreasePlayer1Btn;
+    private View layout;
+    private TextView player1TextView;
+    private TextView player2TextView;
+    private TextView player3TextView;
+    private TextView player4TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,37 +33,40 @@ public class MainActivity extends AppCompatActivity {
         int display_mode = getResources().getConfiguration().orientation;
 
         if (display_mode == Configuration.ORIENTATION_PORTRAIT) {
-            Log.d("lab3", "Portrait");
+            Log.d("display_mode", "Portrait");
         } else {
-            Log.d("lab3", "land");
+            Log.d("display_mode", "land");
             initLandInstance();
         }
     }
 
     private void initLandInstance() {
-        increasePlayer1Btn = (Button) findViewById(R.id.inc_1_btn);
-        decreasePlayer1Btn = (Button) findViewById(R.id.dec_1_btn);
-
-        Log.d("btn", increasePlayer1Btn + "");
-
-        increasePlayer1Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                increasePoint(view);
-            }
-        });
+        layout = findViewById(R.id.layout);
+        player1TextView = (TextView) findViewById(R.id.tv_player1);
+        player2TextView = (TextView) findViewById(R.id.tv_player2);
+        player3TextView = (TextView) findViewById(R.id.tv_player3);
+        player4TextView = (TextView) findViewById(R.id.tv_player4);
     }
 
-    private void increasePoint(View view){
+    public void increasePoint(View view){
         String tag = (String) view.getTag();
-        Log.d("click btn", tag);
-        View layout = findViewById(R.id.layout);
+        Log.d("click inc", tag);
 
         TextView textView = layout.findViewWithTag(tag);
         int score = Integer.parseInt(textView.getText().toString());
         score++;
         textView.setText(score + "");
-        Log.d("lab3", score + "");
+    }
+
+    public void decreasePoint(View view){
+        String tag = (String) view.getTag();
+        Log.d("click dec", tag);
+        View layout = findViewById(R.id.layout);
+
+        TextView textView = layout.findViewWithTag(tag);
+        int score = Integer.parseInt(textView.getText().toString());
+        score--;
+        textView.setText(score + "");
     }
 
     @Override
@@ -66,14 +77,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         if(item.getItemId() == R.id.menu_20){
             Log.d("clickMenu", "20");
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Are you sure?")
                     .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             Log.d("clickMenu", "yes");
+                            resetScore(20);
                         }
                     })
                     .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -86,7 +98,28 @@ public class MainActivity extends AppCompatActivity {
 
         }else if(item.getItemId() == R.id.menu_40){
             Log.d("clickMenu", "40");
+            builder.setTitle("Are you sure?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            resetScore(40);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .create().show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void resetScore(int score){
+        player1TextView.setText(score + "");
+        player2TextView.setText(score + "");
+        player3TextView.setText(score + "");
+        player4TextView.setText(score + "");
     }
 }
